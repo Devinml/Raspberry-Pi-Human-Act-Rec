a5d3afde18f0014542e7486390cb8ba8f951919a
Devinml <devinlink8@gmail.com>
2020-09-14 13:50:23 -0600

    added merge directory

---
+++ src/merge_data.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+
+
+def loop_dir(directory):
+    fp = directory + '/data'
+    data = os.listdir(fp)
+    exp_num = 0
+    out_df = pd.DataFrame(columns=['X', 'Y', 'Z', 'alpha', 'gamma', 'beta', 'activity','test', 'exp_num'])
+    for file_ in data:
+        if 'txt' in file_:
+            b = pd.read_csv(fp+f'/{file_}')
+            b.columns = ['X', 'Y', 'Z', 'alpha', 'gamma', 'beta', 'activity','test']
+            b['exp_num'] = exp_num
+            out_df = pd.concat([out_df, b])
+            exp_num += 1
+    return out_df
+
+
+
+if __name__ == '__main__':
+    root = '/home/devin/Documents/Galvanize/repos/'
+    repo = 'Raspberry-Pi-Human-Act-Rec'
+    directory = root + repo
+    out_df = loop_dir(directory)
+    
